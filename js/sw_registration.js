f79ac9052a0cf94a4b7c00c57ca1736ef4014e3a
vksthomas <vksthomas@gmail.com>
2019-01-25 00:27:18 +0530

    removing / from server-worker.js

--- js/sw_registration.js
+++ js/sw_registration.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
    
-    navigator.serviceWorker.register(`/service-worker.js`).then(function(registration) {
+    navigator.serviceWorker.register(`service-worker.js`).then(function(registration) {
       console.log('Service worker registration succeeded:', registration);
     },  function(error) {
       console.log('Service worker registration failed:', error);
