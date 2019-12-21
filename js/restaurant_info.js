d54b6cf542dceed5737aff773f1e4adbeaf860fc
Vikas Sahu <37984860+vksthomas@users.noreply.github.com>
2019-12-21 12:12:32 +0530

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -24,7 +24,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token=pk.eyJ1IjoidmtzdGhvbWFzIiwiYSI6ImNqcXh1MWd2dzBuM2gzeHF1anJraWg0amkifQ.U8ZcUs_GwI3p9esGFMnQkA', {
-        mapboxToken: 'pk.eyJ1IjoidmtzdGhvbWFzIiwiYSI6ImNqcXh1MWd2dzBuM2gzeHF1anJraWg0amkifQ.U8ZcUs_GwI3p9esGFMnQkA',
+        mapboxToken: 'pk.eyJ1IjoidmtzdGhvbWFzIiwiYSI6ImNrM3luZWZ1dTEyMXAzc3A2MnN6bzRncjcifQ.loQNwJEvCKZRAlL5CWlO-A',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
